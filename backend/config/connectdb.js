2d33d33dc014efe4e6afd49bffbc757c038ee123
zk3762100@gmail.com <132770259+zeeshandeveloper3762@users.noreply.github.com>
2024-04-04 09:29:44 +0500

    no change yet

--- backend/config/connectdb.js
+++ backend/config/connectdb.js
@@ -13,3 +13,4 @@ const connectDB = async (DATABASE_URL) => {
 }
 
 export default connectDB
+
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,28 +1,28 @@
-import { Button, CssBaseline, Grid, Typography } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { getToken, removeToken } from '../services/LocalStorageService';
-import ChangePassword from './auth/ChangePassword';
-import { useGetLoggedUserQuery } from '../services/userAuthApi';
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { setUserInfo, unsetUserInfo } from '../features/userSlice';
-import { unsetUserToken } from '../features/authSlice';
+import { Button, CssBaseline, Grid, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { getToken, removeToken } from "../services/LocalStorageService";
+import ChangePassword from "./auth/ChangePassword";
+import { useGetLoggedUserQuery } from "../services/userAuthApi";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setUserInfo, unsetUserInfo } from "../features/userSlice";
+import { unsetUserToken } from "../features/authSlice";
 const Dashboard = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleLogout = () => {
-    dispatch(unsetUserToken({ token: null }))
-    dispatch(unsetUserInfo({ name: "", email: "" }))
-    removeToken('token')
-    navigate('/login')
-  }
+    dispatch(unsetUserToken({ token: null }));
+    dispatch(unsetUserInfo({ name: "", email: "" }));
+    removeToken("token");
+    navigate("/login");
+  };
 
-  const token = getToken()
-  const { data, isSuccess } = useGetLoggedUserQuery(token)
+  const token = getToken();
+  const { data, isSuccess } = useGetLoggedUserQuery(token);
 
   const [userData, setUserData] = useState({
     email: "",
-    name: ""
-  })
+    name: "",
+  });
 
   // Store User Data in Local State
   useEffect(() => {
@@ -30,35 +30,51 @@ const Dashboard = () => {
       setUserData({
         email: data.user.email,
         name: data.user.name,
-      })
+      });
     }
-  }, [data, isSuccess])
+  }, [data, isSuccess]);
 
   // Store User Data in Redux Store
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
     if (data && isSuccess) {
-      dispatch(setUserInfo({
-        email: data.user.email,
-        name: data.user.name
-      }))
+      dispatch(
+        setUserInfo({
+          email: data.user.email,
+          name: data.user.name,
+        })
+      );
     }
-  }, [data, isSuccess, dispatch])
+  }, [data, isSuccess, dispatch]);
 
-  return <>
-    <CssBaseline />
-    <Grid container>
-      <Grid item sm={4} sx={{ backgroundColor: 'gray', p: 5, color: 'white' }}>
-        <h1>Dashboard</h1>
-        <Typography variant='h5'>Email: {userData.email}</Typography>
-        <Typography variant='h6'>Name: {userData.name}</Typography>
-        <Button variant='contained' color='warning' size='large' onClick={handleLogout} sx={{ mt: 8 }}>Logout</Button>
+  return (
+    <>
+      <CssBaseline />
+      <Grid container>
+        <Grid
+          item
+          sm={4}
+          sx={{ backgroundColor: "gray", p: 5, color: "white" }}
+        >
+          <h1>Dashboard</h1>
+          <Typography variant="h5">Email: {userData.email}</Typography>
+          <Typography variant="h6">Name: {userData.name}</Typography>
+          <Button
+            variant="contained"
+            color="warning"
+            size="large"
+            onClick={handleLogout}
+            sx={{ mt: 8 }}
+          >
+            Logout
+          </Button>
+        </Grid>
+        <Grid item sm={8}>
+          <ChangePassword />
+        </Grid>
       </Grid>
-      <Grid item sm={8}>
-        <ChangePassword />
-      </Grid>
-    </Grid>
-  </>;
+    </>
+  );
 };
 
 export default Dashboard;
